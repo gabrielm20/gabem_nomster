f4d409ce74908fb62b676370ee0c067b15776b18
gabrielm20 <gabrielmaestas20@gmail.com>
2019-05-06 17:34:40 +0000

    trying to add env key

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@
 
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :name, length: { minimum: 3 }
   validates :name, :address, :description, presence: true
 end
