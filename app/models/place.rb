a6a5084537d2b38900f3f3f0f17c0d44aa5ee44e
gabrielm20 <gabrielmaestas20@gmail.com>
2019-05-06 22:05:16 +0000

    added comment sections

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,11 @@
 
 class Place < ApplicationRecord
   belongs_to :user
+  has many :comments
+
   validates :name, length: { minimum: 3 }
   validates :name, :address, :description, presence: true
+  
   geocoded_by :address
   after_validation :geocode
 end
