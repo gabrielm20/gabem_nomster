60191c1e7d628d2f6da096b5352fc82300a4d274
gabrielm20 <gabrielmaestas20@gmail.com>
2019-05-06 22:11:17 +0000

    fixed comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,11 +2,11 @@
 
 class Place < ApplicationRecord
   belongs_to :user
-  has many :comments
+  has_many :comments
 
   validates :name, length: { minimum: 3 }
   validates :name, :address, :description, presence: true
-  
+
   geocoded_by :address
   after_validation :geocode
 end
